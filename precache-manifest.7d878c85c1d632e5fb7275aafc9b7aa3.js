a3c5862733a3e56773adbc88a0f5597add701524
cr57.jayson <cr57.jayson@enfrausaph.com>
2019-01-04 03:03:18 +0800

    Updates

---
+++ precache-manifest.7d878c85c1d632e5fb7275aafc9b7aa3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66e01a8977a9d2cce54a",
+    "url": "/react-crud/static/js/runtime~main.66e01a89.js"
+  },
+  {
+    "revision": "573a90d3035656ae6d31",
+    "url": "/react-crud/static/js/main.573a90d3.chunk.js"
+  },
+  {
+    "revision": "3da870a3c1c4b41662f2",
+    "url": "/react-crud/static/js/1.3da870a3.chunk.js"
+  },
+  {
+    "revision": "573a90d3035656ae6d31",
+    "url": "/react-crud/static/css/main.2fd11052.chunk.css"
+  },
+  {
+    "revision": "e915515f033199fe2cf8a78cf9023bd2",
+    "url": "/react-crud/index.html"
+  }
+];
